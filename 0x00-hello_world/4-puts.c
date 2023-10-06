e0cc847ad686ee4b8cf1d62c23f76acc5c0fc8b0
nameisunknown <orangekapital@gmail.com>
2023-10-06 10:45:25 -0700

    a C program that prints exatcly Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	char outp[] = "Programming is like building a multilingual puzzle";
+	puts(outp);
+	return (0);
+}
