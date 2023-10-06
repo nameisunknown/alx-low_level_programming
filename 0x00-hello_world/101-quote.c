e1566cd04574b9a7b92344dbee31fdd9fa4123fb
nameisunknown <orangekapital@gmail.com>
2023-10-06 16:58:35 -0700

    C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
    You are not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
    Your program should return 1
    Your program should compile without any warnings when using the -Wall gcc option

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
