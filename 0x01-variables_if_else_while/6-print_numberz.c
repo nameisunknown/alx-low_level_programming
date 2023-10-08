c8006f4da1513b6fdcbbe5c0317c482eacc49a4b
nameisunknown <orangekapital@gmail.com>
2023-10-08 02:07:14 -0700

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
    You are not allowed to use any variable of type char
    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
    You can only use putchar twice in your code
    All your code should be in the main function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints 0 - 9 with putchar
+ *
+ * Rturn: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int pq;
+
+	int num = 10;
+
+	for (pq = 0; pq < num; pq++)
+	{
+		putchar(pq);
+	}
+	putchar('\n');
+	return (0);
+}
