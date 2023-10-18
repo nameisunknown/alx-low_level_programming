9a6e406d1cb9bbfe76aed20999681551940860b4
nameisunknown <144392794+nameisunknown@users.noreply.github.com>
2023-10-18 02:24:40 +0100

    working with pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 /**
- * reset_to_98 - takes a pointer to an int parameter
- * and update its value and its point  to 98
+ * reset_to_98 - takes  aponter to anint parameter
+ * and update its value its point to 98
  *
  * @n: An integer that inputs pointer
  *
- * Return: Nothing
+ * Return - Nothing
 */
-
-	void reset_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
