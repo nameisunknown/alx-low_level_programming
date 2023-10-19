5c049404ee8365ffa1cd77f018d2f28c44ac89bf
nameisunknown <orangekapital@gmail.com>
2023-10-20 00:43:08 +0100

    a function that concatenates two strings. Prototype: char *_strncat(char *dest, char *src, int n);The _strncat function is similar to the _strcat function, except that it will use at most n bytes from src; and src does not need to be null-terminated if it contains n or more bytes Return a pointer to the resulting string dest

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: pointer 1
+ * @src: pointer 2
+ * @n: n bytes
+ * Return: strings 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *result = dest;
+
+	int counter = 0;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (counter < n && *src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		counter++;
+	}
+	return result;
+}
