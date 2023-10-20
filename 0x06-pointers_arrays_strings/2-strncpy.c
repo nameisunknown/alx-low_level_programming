c338f4d44652bee0a0ee8a841f3e7bf17759b404
nameisunknown <orangekapital@gmail.com>
2023-10-20 01:39:47 +0100

    a function that copies a string.Prototype: char *_strncpy(char *dest, char *src, int n);Your function should work exactly like strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: pointer 1
+ * @src: pointer 2
+ * @n: length to collect from src
+ * Return: pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int counter = 0;
+
+	while (counter < n && *src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		counter++;
+	}
+	return (dest);
+}
