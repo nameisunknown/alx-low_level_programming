6cfca13dd918bbf3d3696024399cabdd8f4e0960
nameisunknown <144392794+nameisunknown@users.noreply.github.com>
2023-11-10 02:08:00 +0100

    Task 3

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
-void print_name_as_is(char *name);
-void print_name_uppercase(char *name);
-void print_elem_hex(int elem);
-void print_elem(int elem);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
 
 #endif
