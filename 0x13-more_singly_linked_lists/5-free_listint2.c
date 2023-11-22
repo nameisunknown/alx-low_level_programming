7b5eb7f0ddf43a8d5a5f82caae716a270d748943
nameisunknown <orangekapital@gmail.com>
2023-11-22 21:56:39 +0100

    a function that frees a listint_t list.
    Prototype: void free_listint2(listint_t **head);
    The function sets the head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t
+ *
+ * @head: a ponter toa pointer
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while ((*head) != NULL)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+}
