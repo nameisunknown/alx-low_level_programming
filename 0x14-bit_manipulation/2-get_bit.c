9676c4301a2b20db90bef24172fe45cf1695f3d6
nameisunknown <orangekapital@gmail.com>
2023-11-24 22:24:27 +0100

    a function that returns the value of a bit at a given index.
    Prototype: int get_bit(unsigned long int n, unsigned int index);
    where index is the index, starting from 0 of the bit you want to get
    Returns: the value of the bit at index index or -1 if an error occured

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int mask;
 
 	if (index >= sizeof(unsigned long int) * 8)
-		return -1;
+		return (-1);
 	mask = 1UL << index;
-	return (n & mask) ? 1 : 0;
+	return ((n & mask) ? 1 : 0);
 }
