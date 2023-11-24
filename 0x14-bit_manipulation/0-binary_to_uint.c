50fa3c0db1d3569d7207d4f07a1f5500b3708128
nameisunknown <orangekapital@gmail.com>
2023-11-24 14:53:15 +0100

    a function that converts a binary number to an unsigned int.
    Prototype: unsigned int binary_to_uint(const char *b);
    where b is pointing to a string of 0 and 1 chars
    Return: the converted number, or 0 if
    there is one or more chars in the string b that is not 0 or 1
    b is NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,36 +15,32 @@ unsigned int binary_to_uint(const char *b)
 {
 	size_t len = strlen(b);
 	unsigned int result = 0;
-	unsigned int value = 1;
-	size_t c = len;
-	size_t count1 = 0;
+	size_t c = 0;
 
 	int *collector = malloc(len * sizeof(int));
 	if (collector == NULL)
-		return (0);
+		return 0;
 
-	while (c-- > 0)
+	while (c < len)
 	{
-		size_t tempC;
 		if (b[c] == '0' || b[c] == '1')
 		{
 			collector[c] = b[c] - '0';
-			count1 = 0;
-			tempC = c;
-			while (count1 < tempC)
-			{
-				value *= 2;
-				count1++;
-			}
-			result = collector[c] * value + result;
-			value = 1;
 		}
 		else
 		{
 			free(collector);
-			return (0);
+			return 0;
 		}
+		c++;
 	}
+	c = 0;
+	while (c < len)
+	{
+		result = (result << 1) | collector[c];
+		c++;
+	}
+
 	free(collector);
-	return (result);
-}	
+	return result;
+}
