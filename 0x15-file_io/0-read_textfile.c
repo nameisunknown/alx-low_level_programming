e0dad42710087ab1d9227f34321452d24a744d3e
nameisunknown <orangekapital@gmail.com>
2023-11-29 17:32:29 +0100

    a function that reads a text file and prints it to the POSIX standard output.
    Prototype: ssize_t read_textfile(const char *filename, size_t letters);
    where letters is the number of letters it should read and print
    returns the actual number of letters it could read and print
    if the file can not be opened or read, return 0
    if filename is NULL return 0
    if write fails or does not write the expected amount of bytes, return 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,34 +7,38 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int filedesc;
+	int fildes, noOfLettersRead;
 
-	size_t collector, count = 0;
-
-	char *str;
+	char *readCollector;
 
 	if (filename == NULL)
 		return (0);
 
-	filedesc = open(filename, O_RDONLY);
+	fildes = open(filename, O_RDONLY);
 
-	if (filedesc == -1)
+	if (fildes == -1)
+		return (0);
+
+	readCollector = malloc(sizeof(char) * letters);
+
+	noOfLettersRead = read(fildes, readCollector, letters);
+
+	if (noOfLettersRead != -1)
 	{
+		if (write(STDOUT_FILENO, readCollector, letters) == -1)
+		{
+			free(readCollector);
+			return (0);
+		}
+	}
+	else
+	{
+		free(readCollector);
 		return (0);
 	}
+	free(readCollector);
+	close(fildes);
+	return (noOfLettersRead);
+	
 
-	str = malloc(sizeof(char) * letters);
-
-	while (count < letters)
-	{
-		collector = read(filedesc, str, letters);
-		count++;
-	}
-	if (str != NULL)
-	{
-		write(STDOUT_FILENO, str, letters);
-	}
-	free(str);
-	close(filedesc);
-	return (collector);
 }
