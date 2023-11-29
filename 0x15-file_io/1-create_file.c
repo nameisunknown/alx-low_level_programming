f28b953180cc5534e4c1792119c1f74cde76cb7b
nameisunknown <orangekapital@gmail.com>
2023-11-29 22:13:02 +0100

    a function that creates a file.
    Prototype: int create_file(const char *filename, char *text_content);
    where filename is the name of the file to create and text_content is a NULL terminated string to write to the file
    Returns: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…)
    The created file must have those permissions: rw-------. If the file already exists, do not change the permissions.
    if the file already exists, truncate it
    if filename is NULL return -1
    if text_content is NULL create an empty file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
+/**
+ * create_file - creates a file and write to it
+ *
+ * @filename: file to be created
+ * @text_content: content to be written to the created file
+ * Return: 1 on success, -1 on failure
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int filedescriptor;
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	filedescriptor = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
 
 	if (filedescriptor != -1)
