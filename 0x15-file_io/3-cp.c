55cbe3057db66f5786a8c909a8d95c8f0ab486a4
nameisunknown <orangekapital@gmail.com>
2023-11-30 13:50:29 +0100

    a program that copies the content of a file to another file.
    Usage: cp file_from file_to
    if the number of argument is not the correct one, exit with code 97 and print Usage: cp file_from file_to, followed by a new line, on the POSIX standard error
    if file_to already exists, truncate it
    if file_from does not exist, or if you can not read it, exit with code 98 and print Error: Can't read from file NAME_OF_THE_FILE, followed by a new line, on the POSIX standard error
    where NAME_OF_THE_FILE is the first argument passed to your program
    if you can not create or if write to file_to fails, exit with code 99 and print Error: Can't write to NAME_OF_THE_FILE, followed by a new line, on the POSIX standard error
    where NAME_OF_THE_FILE is the second argument passed to your program
    if you can not close a file descriptor , exit with code 100 and print Error: Can't close fd FD_VALUE, followed by a new line, on the POSIX standard error
    where FD_VALUE is the value of the file descriptor
    Permissions of the created file: rw-rw-r--. If the file already exists, do not change the permissions
    You must read 1,024 bytes at a time from the file_from to make less system calls. Use a buffer
    You are allowed to use dprintf

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+int main(int argc, char **argv)
+{
+	int fileDescForFileTo, fileDescForFileFrom;
+
+	char buff[1024];
+
+	if ( argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fileDescForFileTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fileDescForFileFrom = open(argv[1], O_RDONLY);
+
+	if (fileDescForFileFrom == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (fileDescForFileTo == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	read(fileDescForFileFrom, buff, sizeof(buff));
+        write(fileDescForFileTo, buff, sizeof(buff));
+
+	if (close(fileDescForFileTo) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fileDescForFileTo));
+		exit(100);
+	}
+
+	if (close(fileDescForFileFrom) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close(fileDescForFileFrom));
+		exit(100);
+	}
+	return (0);
+}
