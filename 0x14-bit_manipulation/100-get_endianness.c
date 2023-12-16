43947b60211bbee81830574278b880daf35efc5d
nameisunknown <orangekapital@gmail.com>
2023-12-16 06:53:48 +0300

    a function that checks the endianness.
    Prototype: int get_endianness(void);
    Returns: 0 if big endian, 1 if little endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+/**
+ * get_endianness - Checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int n = 1;
+	char *c = (char *) &n;
+
+	return ((int) *c);
+}
